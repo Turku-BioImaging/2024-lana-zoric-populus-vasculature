9a3ff1e5f4d97a1ee903ad9c48430439ab3828d6
Junel Solis <junel.solis@abo.fi>
2024-06-05 16:13:05 +0300

    working ilastik segmentation: - load ilastik project file - declare segmenter class - implement segmentation in main script

---
+++ src/ilastik_segmenter.py
@@ -0,0 +1,39 @@
+"""
+Defines IlastikSegmenter class. Expected input are downsampled images
+of the Populus clones. The output is a rough binary mask of the vascular structures,
+which will require further filtering.
+"""
+
+import os
+from typing import Any, Tuple
+
+import numpy as np
+from ilastik.experimental.api import from_project_file
+from skimage import io  # type: ignore
+from skimage.transform import rescale
+from skimage.util import img_as_ubyte
+from xarray import DataArray
+
+MODEL_DIR = os.path.join(os.path.dirname(__file__), "..", "data", "ilastik_model")
+MODEL_NAME = "model-2024-05-07"
+
+
+class IlastikSegmenter:
+    pipeline = from_project_file(os.path.join(MODEL_DIR, MODEL_NAME + ".ilp"))
+
+    # def __init__(self):
+
+    def segment_one(self, clone_data: Tuple[str, str, str]):
+        data_dir, clone, sample = clone_data
+        img = io.imread(os.path.join(data_dir, clone, sample, "raw_data.tif"))
+        assert img.ndim == 3, img.shape[2] == 3
+
+        img: np.ndarray = img_as_ubyte(
+            rescale(img, 0.5, anti_aliasing=True, order=1, channel_axis=2)
+        )
+        data_array: Any = DataArray(img, dims=["y", "x", "c"])
+        pred: np.ndarray = self.pipeline.predict(data_array).values
+        pred: np.ndarray = img_as_ubyte(pred[..., 1] >= 0.5)
+
+        mask_path = os.path.join(data_dir, clone, sample, "binary_mask.tif")
+        io.imsave(mask_path, pred, check_contrast=False)
--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ import argparse
 import os
 from glob import glob
 
+from ilastik_segmenter import IlastikSegmenter
 from raw_data import copy_raw_data_to_output
 from tqdm import tqdm
 
@@ -30,28 +31,48 @@ parser.add_argument(
     "--skip-raw-data", action="store_true", help="Skip copying raw data"
 )
 parser.add_argument(
+    "--skip-ilastik-binary-mask",
+    action="store_true",
+    help="Skip generating binary mask from Ilastik",
+)
+parser.add_argument(
     "--overwrite", type=bool, default=True, help="Overwrite existing images."
 )
 
 args = parser.parse_args()
 
-
+# copy raw data
 clone_dirs = [
     items
     for items in os.listdir(RAW_DATA_DIR)
     if os.path.isdir(os.path.join(RAW_DATA_DIR, items))
 ]
 
-for clone_dir in clone_dirs:
-    sample_names = [
-        os.path.basename(sample_name)
-        for sample_name in glob(os.path.join(RAW_DATA_DIR, clone_dir, "*.tif"))
-    ]
+if args.skip_raw_data is False:
+    for clone_dir in clone_dirs:
+        sample_names = [
+            os.path.basename(sample_name)
+            for sample_name in glob(os.path.join(RAW_DATA_DIR, clone_dir, "*.tif"))
+        ]
 
-    for sample_name in tqdm(sample_names, desc=f"Copying {clone_dir} samples..."):
-        copy_raw_data_to_output(
-            raw_data_dir=args.raw_data_dir,
-            output_dir=args.output_dir,
-            clone_name=clone_dir,
-            sample_name=sample_name,
-        )
+        for sample_name in tqdm(sample_names, desc=f"Copying {clone_dir} samples..."):
+            copy_raw_data_to_output(
+                raw_data_dir=args.raw_data_dir,
+                output_dir=args.output_dir,
+                clone_name=clone_dir,
+                sample_name=sample_name,
+            )
+
+# ilastik segmentation
+clone_data = [
+    (OUTPUT_DIR, clone, sample)
+    for clone in os.listdir(OUTPUT_DIR)
+    if os.path.isdir(os.path.join(OUTPUT_DIR, clone))
+    for sample in os.listdir(os.path.join(OUTPUT_DIR, clone))
+    if os.path.isdir(os.path.join(OUTPUT_DIR, clone, sample))
+]
+
+segmenter = IlastikSegmenter()
+for item in tqdm(clone_data, desc="Segmenting images using Ilastik..."):
+    segmenter.segment_one(item)
+    # break
