7ec06ca990ddfc77ddd85a01b2ac8dc9306861e0
Ramish Bibi <ramish.r.bibi@utu.fi>
2024-06-11 11:06:43 +0300

    upScale in process

--- src/ilastik_segmenter.py
+++ src/ilastik_segmenter.py
@@ -10,7 +10,7 @@ from typing import Any, Tuple
 import numpy as np
 from ilastik.experimental.api import from_project_file
 from skimage import io  # type: ignore
-from skimage.transform import rescale
+from skimage.transform import rescale, resize
 from skimage.util import img_as_ubyte
 from xarray import DataArray
 
@@ -31,23 +31,25 @@ class IlastikSegmenter:
         img = io.imread(os.path.join(data_dir, clone, sample, "raw_data.tif"))
         assert img.ndim == 3, img.shape[2] == 3
 
-       # Get the original image dimensions
-original_shape = img.shape[:2]  # (y, x)
-
-# Upscale the predicted mask back to the original dimensions
-pred_upscaled: np.ndarray = resize(
-    img_as_ubyte(pred_binary), original_shape, anti_aliasing=True, order=0, preserve_range=True
-)
+        img: np.ndarray = img_as_ubyte(
+            rescale(img, 0.5, anti_aliasing=True, order=1, channel_axis=2)
         )
         data_array: Any = DataArray(img, dims=["y", "x", "c"])
         pred: np.ndarray = self.pipeline.predict(data_array).values
         pred: np.ndarray = img_as_ubyte(pred[..., 1] >= 0.5)
         
         # Upscale back to original
-        upimg: np.ndarray = img_as_ubyte(
-            rescale(img, , anti_aliasing=True, order=0, preserve_range=True
-        )
+        # Get the original image dimensions
+        original_shape = img.shape[:2]  # (y, x)
 
+        # Upscale the predicted mask back to the original dimensions
+        pred_upscaled: np.ndarray = resize(
+            img_as_ubyte(pred), original_shape, anti_aliasing=True, order=0, preserve_range=True
+        )
+        # if necessary
+        #pred_upscaled = img_as_ubyte(pred_upscaled)
+        
+        
         # mask_path = os.path.join(data_dir, clone, sample, "binary_mask.tif")
 
-        io.imsave(mask_path, pred, check_contrast=False)
+        io.imsave(mask_path, pred_upscaled, check_contrast=False)
