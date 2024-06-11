1f259aa72ac923ff4cd2ab3fbc72beb3c7f3f056
Junel Solis <junel.solis@abo.fi>
2024-06-11 12:24:48 +0300

    cleaned test_script.py WIP

--- src/postprocess/test_script.py
+++ src/postprocess/test_script.py
@@ -1,164 +1,62 @@
-
-
-# Remove small objects and generate labelled images after new ilastik model for all images
-
 import os
 import pandas as pd
-from skimage import io, img_as_ubyte
+from skimage import io  # type: ignore
+from skimage.util import img_as_ubyte
 from skimage.morphology import remove_small_objects
 from skimage.measure import label, regionprops
+from tqdm import tqdm
+from typing import List, Dict
 
-def process_image(input_image_path, output_image_path, labeled_image_path, min_size):
-    try:
-        # Read the image
-        image = io.imread(input_image_path)
-        
-        # Ensure the image is binary
-        binary_image = image > 0
+DATA_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "data", "outputs")
+MIN_OBJECT_SIZE = 20000
 
-        # Remove small objects
-        filtered_image = remove_small_objects(binary_image, min_size=min_size)
 
-        # Perform connected component labeling on the filtered image
-        labeled_image = label(filtered_image)
-        
-        # Collect sizes of objects in the labeled image
-        properties = regionprops(labeled_image)
-        object_sizes = [{'filename': input_image_path, 'label': prop.label, 'size': prop.area} for prop in properties]
+def process_sample(data_dir: str, clone: str, sample: str) -> List[Dict]:
+    mask = io.imread(os.path.join(data_dir, clone, sample, "binary_mask.tif"))
+    assert mask.ndim == 2
 
-        
-        # Convert images to unsigned 8-bit format for saving
-        filtered_image_ubyte = img_as_ubyte(filtered_image)
-        labeled_image_ubyte = img_as_ubyte(labeled_image)
-        
-        # Save the cleaned image
-        io.imsave(output_image_path, img_as_ubyte(filtered_image))
-        # Save the labeled image
-        io.imsave(labeled_image_path, labeled_image_ubyte)
+    mask = mask > 0
+    filtered_mask = remove_small_objects(mask, min_size=MIN_OBJECT_SIZE)
 
-        print(f"Processed and saved filtered image at: {output_image_path}")
-        print(f"Processed and saved labeled image at: {labeled_image_path}")
-        
-        return object_sizes
+    labeled_image = label(filtered_mask)
 
-    except Exception as e:
-        print(f"Error processing file {input_image_path}: {e}")
-        return []
+    properties = regionprops(labeled_image)
+    object_sizes: List[Dict] = [
+        {
+            "clone": clone,
+            "sample": sample,
+            "label": prop.label,
+            "size_pixels": prop.area,
+        }
+        for prop in properties
+    ]
 
-# # Replace with the path to your folder containing images
-input_folder = os.path.join(
-    os.path.dirname(__file__), "..", "..", "data", "outputs"
-)   
-min_size = 5000  # Minimum size of small objects to be removed, adjust as needed
-csv_output_path = 'object_sizes.csv'  # Path to save the CSV file
+    io.imsave(
+        os.path.join(data_dir, clone, sample, "filtered_binary_mask.tif"),
+        img_as_ubyte(filtered_mask),
+        check_contrast=False,
+    )
+    io.imsave(
+        os.path.join(data_dir, clone, sample, "object_labels.tif"),
+        labeled_image,
+        check_contrast=False,
+    )
+
+    return object_sizes
+
+
+sample_data = [
+    (clone, sample)
+    for clone in os.listdir(DATA_PATH)
+    if os.path.isdir(os.path.join(DATA_PATH, clone))
+    for sample in os.listdir(os.path.join(DATA_PATH, clone))
+    if os.path.isdir(os.path.join(DATA_PATH, clone, sample))
+]
+
 
 all_object_sizes = []
+for item in tqdm(sample_data):
+    all_object_sizes.extend(process_sample(DATA_PATH, *item))
 
-# Process all images in the folder
-
-for dirpath, dirnames, filenames in os.walk(input_folder):
-    for filename in filenames:
-        if filename.lower().endswith(("binary_mask.tif")):
-            input_image_path = os.path.join(dirpath, filename)
-            
-            # Create a new filename to save the cleaned image
-            name, ext = os.path.splitext(filename)
-            filtered_filename = f"filtered_{name}{ext}"
-            labeled_filename = f"label_{name}{ext}"
-            filtered_file_path = os.path.join(dirpath, filtered_filename)
-            labeled_file_path = os.path.join(dirpath, labeled_filename)
-            
-            object_sizes = process_image(input_image_path, filtered_file_path, labeled_file_path, min_size)
-            all_object_sizes.extend(object_sizes)
-
-    
-    
-    # Save object sizes to a CSV file
 df = pd.DataFrame(all_object_sizes)
-df.to_csv(csv_output_path, index=False)
-
-print(f"Object sizes saved to {csv_output_path}")
-
-
-# # remove objects greater than 100k
-
-# import os
-# import pandas as pd
-# from skimage import io, img_as_ubyte
-# from skimage.morphology import remove_small_objects
-# from skimage.measure import label, regionprops
-
-# def process_image(input_image_path, cleaned_image_path, labeled_image_path, min_size, max_size):
-#     try:
-#         # Read the image
-#         image = io.imread(input_image_path)
-        
-#         # Ensure the image is binary
-#         binary_image = image > 0
-
-#         # Remove small objects
-#         cleaned_image = remove_small_objects(binary_image, min_size=min_size)
-
-#         # Perform connected component labeling on the cleaned image
-#         labeled_image = label(cleaned_image)
-
-#         # Collect sizes of objects in the labeled image and remove large objects
-#         properties = regionprops(labeled_image)
-#         object_sizes = []
-#         for prop in properties:
-#             if prop.area <= max_size:
-#                 object_sizes.append({'filename': input_image_path, 'label': prop.label, 'size': prop.area})
-#             else:
-#                 # Set the pixels of large objects to 0 in the cleaned image
-#                 cleaned_image[labeled_image == prop.label] = 0
-
-#         # Re-label the cleaned image after removing large objects
-#         labeled_image = label(cleaned_image)
-
-#         # Convert images to unsigned 8-bit format for saving
-#         cleaned_image_ubyte = img_as_ubyte(cleaned_image)
-#         labeled_image_ubyte = img_as_ubyte(labeled_image)
-
-#         # Save the cleaned image
-#         io.imsave(cleaned_image_path, cleaned_image_ubyte)
-
-#         # Save the labeled image
-#         io.imsave(labeled_image_path, labeled_image_ubyte)
-
-#         print(f"Processed and saved cleaned image at: {cleaned_image_path}")
-#         print(f"Processed and saved labeled image at: {labeled_image_path}")
-
-#         return object_sizes
-
-#     except Exception as e:
-#         print(f"Error processing file {input_image_path}: {e}")
-#         return []
-
-# # Example usage
-# input_folder = '/path/to/your/folder'  # Replace with the path to your folder containing images
-# min_size = 64  # Minimum size of small objects to be removed, adjust as needed
-# max_size = 100000  # Maximum size of objects to be retained
-# csv_output_path = 'object_sizes.csv'  # Path to save the CSV file
-
-# all_object_sizes = []
-
-# for dirpath, dirnames, filenames in os.walk(input_folder):
-#     for filename in filenames:
-#         if filename.lower().endswith((".tif", ".tiff")):
-#             input_image_path = os.path.join(dirpath, filename)
-            
-#             # Create new filenames to save the cleaned and labeled images
-#             name, ext = os.path.splitext(filename)
-#             cleaned_filename = f"cleaned_{name}{ext}"
-#             labeled_filename = f"label_{name}{ext}"
-#             cleaned_file_path = os.path.join(dirpath, cleaned_filename)
-#             labeled_file_path = os.path.join(dirpath, labeled_filename)
-            
-#             object_sizes = process_image(input_image_path, cleaned_file_path, labeled_file_path, min_size, max_size)
-#             all_object_sizes.extend(object_sizes)
-
-# # Save object sizes to a CSV file
-# df = pd.DataFrame(all_object_sizes)
-# df.to_csv(csv_output_path, index=False)
-
-# print(f"Object sizes saved to {csv_output_path}")
+df.to_csv(os.path.join(DATA_PATH, "object_sizes.csv"), index=False)
