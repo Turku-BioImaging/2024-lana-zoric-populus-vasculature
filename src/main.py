bf039c78792f21dccf7f30f5a5041f3786ed59f8
Junel Solis <junel.solis@abo.fi>
2024-07-01 14:01:59 +0300

    refactor code, write raw data to zarr

--- src/main.py
+++ src/main.py
@@ -6,17 +6,16 @@ Turku BioImaging, 2024
 
 import argparse
 import os
-from glob import glob
-from joblib import Parallel, delayed
 
-from ilastik_segmenter import IlastikSegmenter
-from raw_data import copy_raw_data_to_output
-from tqdm import tqdm
+from util.zarr_converter import convert_raw_data_to_zarr
 
 RAW_DATA_DIR = os.path.join(
     os.path.dirname(__file__), "..", "data", "raw_data", "Populus"
 )
-OUTPUT_DIR = os.path.join(os.path.dirname(__file__), "..", "data", "outputs")
+
+ZARR_PATH = os.path.join(
+    os.path.dirname(__file__), "..", "data", "zarr_data", "data.zarr"
+)
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -25,71 +24,16 @@ parser.add_argument(
     default=RAW_DATA_DIR,
     help="Path to the raw data containing directories of Populus clones.",
 )
+
 parser.add_argument(
-    "--output-dir", type=str, default=OUTPUT_DIR, help="Path to save outputs."
+    "--zarr-path", type=str, default=ZARR_PATH, help="Path to save Zarr data."
 )
+
 parser.add_argument(
     "--skip-raw-data", action="store_true", help="Skip copying raw data"
-)
-parser.add_argument(
-    "--skip-ilastik-binary-mask",
-    action="store_true",
-    help="Skip generating binary mask from Ilastik",
-)
-parser.add_argument(
-    "--overwrite", action="store_true", help="Overwrite existing images."
 )
 
 args = parser.parse_args()
 
-# copy raw data
-clone_dirs = [
-    items
-    for items in os.listdir(RAW_DATA_DIR)
-    if os.path.isdir(os.path.join(RAW_DATA_DIR, items))
-]
-
-
-def copy_sample(raw_data_dir, output_dir, clone_name, sample_name, overwrite):
-    copy_raw_data_to_output(
-        raw_data_dir=raw_data_dir,
-        output_dir=output_dir,
-        clone_name=clone_name,
-        sample_name=sample_name,
-        overwrite=overwrite,
-    )
-
-
-if args.skip_raw_data is False:
-    for clone_dir in clone_dirs:
-        sample_names = [
-            os.path.basename(sample_name)
-            for sample_name in glob(os.path.join(RAW_DATA_DIR, clone_dir, "*.tif"))
-        ]
-
-        Parallel(n_jobs=-1)(
-            delayed(copy_sample)(
-                raw_data_dir=args.raw_data_dir,
-                output_dir=args.output_dir,
-                clone_name=clone_dir,
-                sample_name=sample_name,
-                overwrite=args.overwrite,
-            )
-            for sample_name in tqdm(
-                sample_names, desc=f"Copying {clone_dir} samples..."
-            )
-        )
-
-# ilastik segmentation
-clone_data = [
-    (OUTPUT_DIR, clone, sample)
-    for clone in os.listdir(OUTPUT_DIR)
-    if os.path.isdir(os.path.join(OUTPUT_DIR, clone))
-    for sample in os.listdir(os.path.join(OUTPUT_DIR, clone))
-    if os.path.isdir(os.path.join(OUTPUT_DIR, clone, sample))
-]
-
-segmenter = IlastikSegmenter()
-for item in tqdm(clone_data, desc="Segmenting images using Ilastik..."):
-    segmenter.segment_one(item, overwrite=args.overwrite)
-    # break
+if not args.skip_raw_data:
+    convert_raw_data_to_zarr(args.raw_data_dir, args.zarr_path)
